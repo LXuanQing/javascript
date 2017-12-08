9d9210f418f148287654be8994f75cf556f96ae5
lxq <wb-lxq302621@alibaba-inc.com>
2017-12-08 10:20:18 +0800

    ajax

--- demo.js
+++ demo.js
@@ -7,6 +7,9 @@ console.log(!!x)      // false;
 console.log(!!y)      // false;
 console.log(!!str)    // true;
 
+
+
+
 // 2 数字字符串前加+，会强制转number
 var str = "123"
 var num = +str
@@ -14,6 +17,8 @@ console.log(num)
 //但是如果是混合类型的字符串，则会转为NaN
 var b="1606e";
 console.log(+b)     // NaN
+
+
 
 // 3 void 0 或 void (0) === undefined
 // 当undefined被放在在function函数内，我们把它当成一个局部变量，它是可以赋上值的
@@ -44,6 +49,8 @@ if(a===void (0)){
 //最后我们打印一下这两个的返回值
 console.log(void 0,void (0))    // undefined undefined
 
+
+
 // 4 数组sort方法，改变原数组
 // a-b输出从小到大排序，b-a输出从大到小排序
 // 原理：冒泡法排序，往最后冒
@@ -56,3 +63,35 @@ arr.sort(() => {
     return Math.random() - 0.5
 })
 console.log(arr)
+
+
+
+// 5 去除字符串前后、前、后、所有空格
+//  type 1-所有空格，2-前后空格，3-前空格，4-后空格
+var str = " 3 5hgh y6y6  "
+function trim(str,type){
+    switch (type){
+        case 1:return str.replace(/\s+/g,"");
+        case 2:return str.replace(/(^\s*)|(\s*$)/g, "");
+        case 3:return str.replace(/(^\s*)/g, "");
+        case 4:return str.replace(/(\s*$)/g, "");
+        default:return str;
+    }
+}
+console.log(str)
+console.log(trim(str,2))
+
+
+// 5 ajax请求的成功和错误
+$.ajax({
+    url: "",
+    data: {},
+    success: function(content) {
+        console.log(content,"success")
+    },
+    error: function(error,a,b) {
+        console.log(error,a,b,"error")
+    }
+})
+// 只要是200，就会执行成功的回调函数
+// 接口不存在等失败才会调用失败的回调函数
