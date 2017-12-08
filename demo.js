40b9ef2df48c77a3ed296538372081511b8ca560
lxq <wb-lxq302621@alibaba-inc.com>
2017-12-08 11:46:01 +0800

    123

--- demo.js
+++ demo.js
@@ -1,3 +1,4 @@
+
 // 1 强转布尔值
 var x=null;
 var y="";
@@ -82,7 +83,8 @@ console.log(str)
 console.log(trim(str,2))
 
 
-// 5 ajax请求的成功和错误
+// 6 ajax请求的成功和错误
+
 $.ajax({
     url: "",
     data: {},
@@ -93,5 +95,40 @@ $.ajax({
         console.log(error,a,b,"error")
     }
 })
+
 // 只要是200，就会执行成功的回调函数
 // 接口不存在等失败才会调用失败的回调函数
+
+
+// 7 意外的全局变量
+function test() {
+    a = 10
+}
+test()
+// 等同于
+function test() {
+    window.a = 10
+}
+console.log(window.a)
+// 另外，通过this创建的全局变量
+// 当在全局作用域中调用foo函数，此时this指向的是全局对象(window)，而不是'undefined'
+function foo() {
+    this.variable = "potential accidental global";
+}
+
+// 解决方法：
+// 在JavaScript文件中添加'use strict'，开启严格模式，可以有效地避免上述问题。
+function fn() {
+    'use strict'
+    bar = 3 // 报错：因为bar还没有被声明
+}
+fn()
+// 谈到全局变量，需要注意那些用来临时存储大量数据的全局变量，确保在处理完这些数据后将其设置为null或重新赋值。
+// console.log打印的对象是不会被垃圾回收器回收的。
+
+
+// 8 函数自调用
+!function test() {
+    var a = 10
+    // console.log(a)
+}()
